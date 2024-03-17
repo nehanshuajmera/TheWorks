a1217795577302f7ef8e12bbd37d022018a57734
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-03-18 00:16:51 +0530

    Responsive Header Done

--- client/src/universal/header/header.jsx
+++ client/src/universal/header/header.jsx
@@ -1,7 +1,9 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./header.css";
 
 export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <header>
       <div className="nav-background">
@@ -12,37 +14,39 @@ export const Header = () => {
               alt="Loading Error"
             />
           </div>
-          <div className="menu">
+          <div className="menu" onClick={() => { setMenuOpen(!menuOpen)}}>
             <span></span>
             <span></span>
             <span></span>
           </div>
-          <div className="navigation-items">
-            <NavLink to="">
-              <div className="nav-item">Resources</div>
-            </NavLink>
-            <NavLink to="">
-              <div className="nav-item">Membership</div>
-            </NavLink>
-            <NavLink to="">
-              <div className="nav-item">Consultation</div>
-            </NavLink>
-            <NavLink to="">
-              <div className="nav-item">Shop</div>
-            </NavLink>
-            <NavLink to="">
-              <div className="nav-item">About</div>
-            </NavLink>
-          </div>
-          <div className="app-utilities">
-            <div className="search-icon">
-              <img src="/searchIcon.png" alt="Loading Error" />
+          <div className={menuOpen ? "open-menu" : "navigation-items"}>
+            <div className="nav-links">
+              <NavLink to="">
+                <div className="nav-item">Resources</div>
+              </NavLink>
+              <NavLink to="">
+                <div className="nav-item">Membership</div>
+              </NavLink>
+              <NavLink to="">
+                <div className="nav-item">Consultation</div>
+              </NavLink>
+              <NavLink to="">
+                <div className="nav-item">Shop</div>
+              </NavLink>
+              <NavLink to="">
+                <div className="nav-item">About</div>
+              </NavLink>
             </div>
-            <div className="user-icon">
-              <img src="/userIcon.png" alt="Loading Error" />
-            </div>
-            <div className="cart-icon">
-              <img src="cartIcon.png" alt="Loading Error" />
+            <div className="app-utilities">
+              <div className="search-icon">
+                <img src="/searchIcon.png" alt="Loading Error" />
+              </div>
+              <div className="user-icon">
+                <img src="/userIcon.png" alt="Loading Error" />
+              </div>
+              <div className="cart-icon">
+                <img src="cartIcon.png" alt="Loading Error" />
+              </div>
             </div>
           </div>
         </nav>
